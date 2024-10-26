1613277df3108496cad29a44e430c52375ef73ac
osamaarwadi <osamarwadi2@gmail.com>
2024-10-26 20:00:53 +0300

    set up initial to-do items from placeholder api

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,67 +1,85 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 import './ToDoList.css'; 
 
 const ToDoList: React.FC = () => {
 
-    const APItest = async(): Promise<void> => {
+    interface Item {
+        id: string;
+        title: string;
+        completed: boolean;
+    }
+
+    enum FilterType { all, pending, completed };
+
+    const [items, setItems] = useState<Item[]>(() => {
+        return JSON.parse(localStorage.getItem('items') || '[]');    
+    });
+
+    const [filter, setFilter] = useState<FilterType>(FilterType.all);
+
+    useEffect(() => {
+        if (!isLoaded.current) {
+            loadPlaceholderItems();
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('items', JSON.stringify(items));
+    }, [items]);
+    
+    const inputRef = useRef<HTMLInputElement>(null);
+    const isLoaded = useRef<boolean>(localStorage.getItem('initialLoadStatus') === "done");
+
+    const loadPlaceholderItems = async(): Promise<void> => {
         try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-            console.log(response);
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users/1/todos');
+            const newItems: Item[] = response.data.map((item: Item) => ({
+                id: item.id,
+                title: item.title,
+                completed: item.completed,
+            }));
+            setItems(newItems);
+            localStorage.setItem('items', JSON.stringify(newItems));
+            localStorage.setItem('initialLoadStatus', "done");
         } catch (err) {
             console.log(err);
         }
     }
-    
-    interface Item {
-        id: string;
-        text: string;
-        isCompleted: boolean;
+
+    const addItem = (): void => {
+        const newItem: Item = { id: uuidv4(),
+                                title: inputRef.current?.value || "", 
+                                completed: false };
+        if (newItem.title !== "") {
+            setItems(prevItems => [...prevItems, newItem]);
+        }
+        else {
+            console.log("Please enter a task.");
+        }
+        if (inputRef.current) {
+            inputRef.current.value = "";
+        }
     }
 
-    const [items, setItems] = useState<Item[]>(() => {
-        return JSON.parse(localStorage.getItem('items') || '[]');
-    });
-
-    enum FilterType { all, pending, completed };
-
-    const [filter, setFilter] = useState<FilterType>(FilterType.all);
+    const deleteItem = (id: string): void => {
+        setItems(items.filter((item) => item.id !== id));
+    }
+    
+    const toggleCompletion = (id: string): void => {
+        setItems(prevItems => prevItems.map((item) => item.id === id ? { ...item, completed: !item.completed } : item));    
+    }
 
     const filterItems = (): Item[] => {
         switch (filter) {
             case FilterType.all:
                 return items;
             case FilterType.pending:
-                return items.filter(item => !item.isCompleted);
+                return items.filter(item => !item.completed);
             case FilterType.completed:
-                return items.filter(item => item.isCompleted);
+                return items.filter(item => item.completed);
         }
-    }
-
-    const addItem = (): void => {
-        const newItem: Item = { id: uuidv4(),
-                                text: (document.getElementById("itemTextBox") as HTMLInputElement).value, 
-                                isCompleted: false };
-        if (newItem.text !== "") {
-            setItems(prevItems => [...prevItems, newItem]);
-        }
-        else {
-            console.log("Please enter a task.");
-        }
-        (document.getElementById("itemTextBox") as HTMLInputElement).value = "";
-    }
-
-    useEffect(() => {
-        localStorage.setItem('items', JSON.stringify(items));
-    }, [items]);
-    
-    const deleteItem = (id: string): void => {
-        setItems(items.filter((item) => item.id !== id));
-    }
-    
-    const toggleCompletion = (id: string): void => {
-        setItems(prevItems => prevItems.map((item) => item.id === id ? { ...item, isCompleted: !item.isCompleted } : item));    
     }
 
     const updateFilter = (event: React.ChangeEvent<HTMLSelectElement>): void => {
@@ -71,12 +89,12 @@ const ToDoList: React.FC = () => {
     return (
     <div>
         <div>
-            <input type="text" id="itemTextBox"></input>
+            <input type="text" ref={ inputRef }></input>
             <button onClick={addItem}>Add item</button>
         </div>
         <div>
             <label htmlFor="filter">Filter: </label>
-            <select name="filter" onChange={(e) => updateFilter(e)}>
+            <select id="filter" onChange={(e) => updateFilter(e)}>
                 <option value="all">Show All</option>
                 <option value="pending">Pending only</option>
                 <option value="completed">Completed only</option>
@@ -88,10 +106,10 @@ const ToDoList: React.FC = () => {
                 <li key={item.id}>
                     <input
                         type="checkbox" 
-                        checked={item.isCompleted}
+                        checked={item.completed}
                         onChange={() => toggleCompletion(item.id)}
                     />
-                    <span className={item.isCompleted ? "completed-item" : ""}> {item.text} </span>
+                    <span className={item.completed ? "completed-item" : ""}> {item.title} </span>
                     <button onClick={() => deleteItem(item.id)}>❌</button>
                 </li>
             )}
